228c14a6142d4c9078c671dd555ddfe0d2fc5558
algol60 <algol60@users.noreply.github.com>
2024-12-15 18:23:45 +1100

    Use ':' in entry point keys.

--- src/sier2_blocks/shelf/__init__.py
+++ src/sier2_blocks/shelf/__init__.py
@@ -2,13 +2,13 @@ from sier2 import Info
 
 def blocks() -> list[Info]:
     return [
-        Info('sier2_blocks.blocks.LoadDataFrame', 'Load a dataframe from a file'),
-        Info('sier2_blocks.blocks.SimpleTable', 'Display a simple table'),
-        Info('sier2_blocks.blocks.HvPoints', 'A Holoviews Points chart')
+        Info('sier2_blocks.blocks:LoadDataFrame', 'Load a dataframe from a file'),
+        Info('sier2_blocks.blocks:SimpleTable', 'Display a simple table'),
+        Info('sier2_blocks.blocks:HvPoints', 'A Holoviews Points chart')
     ]
 
 def dags() -> list[Info]:
     return [
-        Info('sier2_blocks.dags.hv_points_dag', 'Load and plot a dataframe as points'),
-        Info('sier2_blocks.dags.table_view_dag', 'Load a dataframe from file and display in a panel table'),
+        Info('sier2_blocks.dags:hv_points_dag', 'Load and plot a dataframe as points'),
+        Info('sier2_blocks.dags:table_view_dag', 'Load a dataframe from file and display in a panel table'),
     ]
